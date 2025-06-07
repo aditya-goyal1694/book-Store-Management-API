315fdbdecfa8b158e363bef908a01a4dc3172381
Aditya Goyal <adityamr.1694@gmail.com>
2025-06-07 16:35:05 +0530

    typo fixed

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -3,13 +3,19 @@ package utils
 import (
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 )
 
-func ParseBody(r *http.Request, x interface{}){
-	if body, err := io.ReadAll(r.Body); err == nil {
-		if err := json.Unmarshal([]byte(body), x); err != nil {
-			return
-		}
+func ParseBody(r *http.Request, x interface{}) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error reading HTTP request body: %v", err)
+		return
+	}
+	err = json.Unmarshal(body, x)
+	if err != nil {
+		log.Printf("Error unmarshalling JSON: %v", err)
+		return
 	}
 }
